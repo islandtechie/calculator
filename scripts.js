dd4af0b063190f0ed74e62a5ab3fd80e5c0ef81c
Berlin Smith <berlin.smithjr@gmail.com>
2019-06-27 01:23:37 -0400

    Working on numberpad and display functionality

--- scripts.js
+++ scripts.js
@@ -1,126 +1,87 @@
-//Called onload event to init Calculator
-window.onload = function (e) {
-    //alert('Window Object Loaded!');
-    initCalculaltor();
-}
-
-function initCalculaltor() {
-
-    var calculator = new Calculator();
-
-    //Init function buttons
-    var functionButtons = document.querySelectorAll('#function-buttons button');
-    functionButtons.forEach(function(el) {
-        switch (el.id) {
-            case 'save':
-                el.addEventListener('click', calculator.save);
-                break;
-            case 'recall':
-                el.addEventListener('click', calculator.recall);
-                break;
-            case 'clear-input':
-                el.addEventListener('click', calculator.clearInput);
-                break;
-            case 'clear':
-                el.addEventListener('click', calculator.clear);
-
-        }
-    });
-
-    //Init Numpad Buttons    
-    var numpad = document.querySelectorAll('#numpad button');
-    numpad.forEach(function(el) {
-        el.addEventListener('click', function(el){
-            calculator.parseNumber(this);
-        });
-    });
-
-    //Init Operator Buttons
-    var operator = document.querySelectorAll('#operators button');
-    operator.forEach(function(el) {
-        switch (el.id) {
-            case 'add':
-                el.addEventListener('click', calculator.add);
-                break;
-            case 'subtract':
-                el.addEventListener('click', calculator.subtract);
-                break;
-            case 'divde':
-                el.addEventListener('click', calculator.divide);
-                break;
-            case 'multiply':
-                el.addEventListener('click', calculator.multiply);
-
-        }
-    });
-
-    calculator.updateInput(0);
-    
-}
-
 function Calculator()
 {
-    this.currentOperand = 0;
-    this.nextOperand = 0;
+    this.currentOperand = '0';
+    this.nextOperand = '';
     this.memSave = 0;
     this.memClear = 0;
     this.memRecall = 0;
 
-    this.add = function()
-    {
-        console.log('this the add function');
+    
+}
+
+
+var calculator = new Calculator();
+
+var display = document.getElementById('display');
+
+//init Display value
+display.value = calculator.currentOperand;
+console.log(display.value);
+
+//Init function buttons
+var functionButtons = document.querySelectorAll('#function-buttons button');
+functionButtons.forEach(function(el) {
+    switch (el.id) {
+        case 'save':
+            el.addEventListener('click', calculator.save);
+            break;
+        case 'recall':
+            el.addEventListener('click', calculator.recall);
+            break;
+        case 'clear-input':
+            el.addEventListener('click', calculator.clearInput);
+            break;
+        case 'clear':
+            el.addEventListener('click', calculator.clear);
+
+    }
+});
+
+//Init Numpad Buttons    
+var numpad = document.querySelectorAll('#numpad button');
+numpad.forEach(function(el) {
+    el.addEventListener('click', function(el){
+        parseInput(this.id);
+    });
+});
+
+//Init Operator Buttons
+var operator = document.querySelectorAll('#operators button');
+operator.forEach(function(el) {
+    switch (el.id) {
+        case 'add':
+            el.addEventListener('click', calculator.add);
+            break;
+        case 'subtract':
+            el.addEventListener('click', calculator.subtract);
+            break;
+        case 'divde':
+            el.addEventListener('click', calculator.divide);
+            break;
+        case 'multiply':
+            el.addEventListener('click', calculator.multiply);
+
+    }
+});
+
+function parseInput(input)
+{    
+    if (input === '.' && !calculator.currentOperand.includes('.')) {
+             calculator.currentOperand += input;
     }
 
-    this.subtract = function()
-    {
-        console.log('this the subtract function');
+    if (calculator.currentOperand == '0' && calculator.currentOperand.length == 1) {
+        if (input !== '0' || input == '0') {
+            calculator.currentOperand = input;
+        }       
+    }else{
+        calculator.currentOperand += input;
     }
 
-    this.multiply = function()
-    {
-        console.log('this the multiply function');
-    }
+    updateDisplay();
+}
 
-    this.divide = function()
-    {
-        console.log('this the add function');
-    }
-
-    this.save = function()
-    {
-        console.log('this the save function');
-    }
-
-    this.clear = function()
-    {
-        console.log('this the clear function');
-    }
-
-    this.recall = function()
-    {
-        console.log('this the recall function');
-    }
-
-    this.parseNumber = function(el)
-    {
-        this.updateInput(el.id);
-    }
-
-    this.updateInput = function(number)
-    {
-        var input = document.getElementById('display');
-
-        if (input.value == '0' && input.value.length == 1)
-        {
-            input.value = number;
-        }else{
-            input.value += number;
-        }
-        
-        this.currentOperand = parseInt(input.value);
-        console.log(this.currentOperand);
-        console.log(input.value.length);
-
-        
-    }
+function updateDisplay()
+{
+    display.value = calculator.currentOperand;
 }
